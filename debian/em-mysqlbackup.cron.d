ff5340cc958713783c1f2f68f7e052a4f6a9863d
Volodymyr Kolesnykov <sjinks@users.noreply.github.com>
2015-11-12 15:13:21 +0200

    Update em-mysqlbackup.cron.d
    Remove mysqlcheck from cron

--- debian/em-mysqlbackup.cron.d
+++ debian/em-mysqlbackup.cron.d
@@ -1,2 +1,1 @@
-3 * * * *	root	[ -x /usr/bin/mysqlcheck ] && /usr/bin/mysqlcheck --defaults-file=/etc/mysql/debian.cnf -CsA
 18 */6 * * *	root	[ -x /usr/bin/em-mysqlbackup ] && /usr/bin/em-mysqlbackup
